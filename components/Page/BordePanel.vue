59848c6f74f1a69963c919505a697e2a55197add
Posoroko <eric@posoroko;com>
2023-08-02 21:32:44 +0200

    ingredients, with search

--- components/Page/BordePanel.vue
+++ components/Page/BordePanel.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
     <div class="borderPanel bodyText">
         <slot name="content"></slot>
--- pages/verre-doseur.vue
+++ pages/verre-doseur.vue
@@ -12,13 +12,21 @@ const fetchOptions = {
 const { data: items } = await useAsyncData(
     "ingredients",
     async () => {
-        const items = await $fetch(`${directusItems}Tutu_Ingredients?limit=10`, fetchOptions)
+        const items = await $fetch(`${directusItems}Tutu_Ingredients`, fetchOptions)
 
         return items.data
     }
     ,
     { server: false }
 )
+const queryString = ref('');
+
+const list = computed(() => {
+    return items.value.filter((item) => {
+        return item.name.toLowerCase().includes(queryString.value.toLowerCase());
+    })
+})
+
 </script>
 
 <template>
@@ -28,10 +36,33 @@ const { data: items } = await useAsyncData(
         </template>
 
         <template #main>
-            <div class="ingredientsContent flex column gap20 absoluteFull b">
-                <div v-for="item in items" :key="item.id">
-                    <p>{{ item.name }}</p>
-                    <p>{{ item.perLitre }}gr</p>
+            <div class="absoluteFull flex column pad5">
+                <div class="searchBox flex justifyEnd">
+                    <input class="input" type="text" v-model="queryString" placeholder="Rechercher un ingrédient" />
+                </div>
+
+                <div class="ingredientsContent flex column gap20 ">
+
+                    <div class="topTableBox w100 flex">
+                        <p class="w40 bodyText column1">ingrédient</p>
+
+                        <div class="flex grow bodyText">
+                            <p class="grow centered">1/2 litre</p>
+                            <p class="grow centered">1/4 litre</p>
+                            <p class="grow centered">1 litre</p>
+                        </div>
+                    </div>
+
+
+                    <div class="flex alignEnd justifyBetween bodyText" v-for="item in list" :key="item.id">
+                        <p class="w40 column1 item">{{ item.name }}</p>
+                        
+                        <div class="gramsBox flex grow">
+                            <p class="grams centered">{{ Math.floor(item.perLiter / 4)}}gr</p>
+                            <p class="grams centered">{{ Math.floor(item.perLiter / 2) }}gr</p>
+                            <p class="grams centered">{{ item.perLiter }}gr</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </template>
@@ -43,8 +74,41 @@ const { data: items } = await useAsyncData(
 </template>
 
 <style scoped>
+.searchBox {
+    background-color: var(--brand-color-1);
+    padding: 4px;
+    z-index: 1;
+    
+}
+.input {
+    font-family: var(--text-font);
+    font-size: 20px;
+    color: var(--brand-color-2);
+    padding: 7px;
+    background-color: var(--basic-light-color);
+    border: none;
+    outline: none;
+    flex-grow: 1;
+}
+.topTableBox{
+    padding: 4px;
+    border: 4px solid var(--brand-color-1);
+    margin-top: 5px;
+}
 .ingredientsContent {
     overflow: scroll;
+    flex-grow: 1;
+}
+.column1 {
+    padding-left: 10px;
+}
+.column1.item {
+    padding-left: 20px;
+}
+.gramsBox {
+    flex-grow: 1;
+}
+.grams {
     flex-grow: 1;
 }
 </style>
