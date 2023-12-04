ed0967fe1128bee46c5edc7310b08cf30f441c60
Posoroko <eric@posoroko;com>
2023-12-04 21:47:55 +0100

    manifets link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,21 @@ export default defineNuxtConfig({
         },
         preconnect: true,
         useStylesheet: true
-    }
+    },
+     app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            // titleTemplate: '%pageTitle %titleSeparator %siteName',
+            title: 'Demande à Tutu, il a la réponse !',
+            htmlAttrs: {
+                lang: 'fr'
+            },
+
+            link: [
+                { rel: "manifest", href: "/site.webmanifest" }
+            ],
+           
+        }
+    },
 })
